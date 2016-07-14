116fe02870ef59d0ffd832db3a16621cba19c26c
T. Robert Ward <trironyard@icloud.com>
2016-07-14 18:17:22 -0400

    works, needs cleanup and features added

---
+++ mystery_word.py
@@ -0,0 +1,121 @@
+#W1D4 Hangman   235,886
+
+import time
+import random
+
+word_list = []
+alive = True
+
+with open('/usr/share/dict/words','r') as f:
+    for line in f:
+        word_list.append(line)
+
+
+easy_words = []
+normal_words = []
+hard_words = []
+game_list = []
+
+for each in word_list:
+    if len(each) > 4 and len(each) < 8:
+        easy_words.append(each)
+for each in word_list:
+    if len(each) > 6 and len(each) < 10:
+        normal_words.append(each)
+for each in word_list:
+    if len(each) > 8:
+        hard_words.append(each)
+
+
+def word_difficulty():
+    entry = input("Select difficulty level: E, N, H:  ").lower()
+    if entry == 'e':
+        return easy_words
+    elif entry == 'n':
+        return normal_words
+    elif entry == 'h':
+        return hard_words
+
+
+def get_myst_word(game_list):
+    x = len(game_list)
+    y = random.randint(1,x)
+    return game_list[y]
+
+
+def display_board(guesses_left):
+    print("\n"*50)
+    print(myst_word)
+    print("\t",end='')
+    for each in letters_to_display:
+        print(each,end='')
+    print("\n")
+
+    print("\t",end='')
+    for each in mwl:
+        print("_ ",end='')
+    print("\t\t\t*** You have {} guesses left. ***".format(guesses_left))
+    print("\n\n")
+    return
+
+
+def get_guess():
+    while True:
+        entry = input("\n Guess a letter: ").lower()
+        if entry not in all_guesses:
+            all_guesses.append(entry)
+            return entry
+        print("You already guessed that letter  -  try again\n")
+
+
+def check_guess(g):
+    position_of_corr = []
+    for index, each in enumerate(mwl):
+        if each == g:
+            position_of_corr.append(index)
+    return position_of_corr
+
+
+def change_display_letters(guess):
+    for each in position_of_corr:
+        pos = each * 2
+        letters_to_display.insert(pos,guess)
+        letters_to_display.pop(pos+1)
+
+
+print("\n"*50)
+print("WELCOME")
+
+game_list = word_difficulty()
+
+myst_word = get_myst_word(game_list)
+myst_word = myst_word.lower()
+print(myst_word)
+mwl = list(myst_word)
+mwl.pop()
+letters_to_display = []
+for each in mwl:
+    letters_to_display.append('#')
+    letters_to_display.append(' ')
+all_guesses = []
+position_of_corr = []
+guesses_left = 8
+extra_guess = False
+
+while alive:
+
+    display_board(guesses_left)
+
+    guess = get_guess()
+
+    position_of_corr = check_guess(guess)
+
+    change_display_letters(guess)
+
+    if position_of_corr == []:
+        guesses_left -= 1
+
+    if guesses_left <= 0:
+        alive = False
+
+# ending()
